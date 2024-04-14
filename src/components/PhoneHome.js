26a0b9df4c2699ff84eca2eecb4abb9235f7ede4
yoonseo232 <ys.bang232@gmail.com>
2024-04-15 08:05:08 +0900

    핸드폰 화면 성공적이게 띄우기

--- src/components/PhoneHome.js
+++ src/components/PhoneHome.js
@@ -3,13 +3,15 @@ import "./css/story.css";
 import "./css/phoneHome.css";
 
 export default function phoneHome(){
-    // window.location.href = `/detail/phoneHome`
     return (
+        <div className="background">
+
         <div class="app">
             <a href="phone_phoneNumber.html"><button><img src="./images/phone_address.png" alt="" width="70px"/></button></a>
             <a href=""><button><img src="./images/phone_call.png" alt="" width="70px"/></button></a>
             <a href="message.html"><button><img src="./images/phone_message.png" alt="" width="70px"/></button></a>
             <a href="gallery.html"><button><img src="./images/phone_gallery.png" alt="" width="70px"/></button></a>
+        </div>
         </div>
     );
 }
