71a7c69e0eb69528ecb4b939e0c25e4fe0d3b2a7
hxxjx523 <hhj05230@gmail.com>
2024-04-16 08:33:38 +0900

    김여주

--- src/components/story1.js
+++ src/components/story1.js
@@ -7,6 +7,8 @@ export default function Story(){
             <div className="kimyeoju">
                 <img src="./images/Kimyeoju_face1.png" className="kimyeojuImg"/>
                 <img src="./images/Kimyeoju_dialogueWindow1.png" className="dialogueWindow1"/>
+                <div className="name">김여주</div>
+                <div className="dialogue">안녕하세요?</div>
             </div>
         </div>
     )
