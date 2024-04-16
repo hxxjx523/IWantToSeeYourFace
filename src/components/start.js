d95d63af4655c3a242fdc76727a6ad91077f6346
hxxjx523 <hhj05230@gmail.com>
2024-04-16 15:39:37 +0900

    0416

--- src/components/start.js
+++ src/components/start.js
@@ -1,28 +1,51 @@
 import "./css/all.css";
 import "./css/start.css";
 import { useNavigate } from "react-router-dom";
-// import { useState } from "react";
+import { useState } from "react";
 
 export default function Start() {
     const movePage = useNavigate();
   
+    // const Loading = () => {
+    //   setTimeout(()=>{
+    //     movePage('/question');
+    //   },2000);
+    //   return <div className="loading"></div>
+      
+    // }
     
+    // return (
+    //   <div id="background">
+    //     <div className="btn">
+    //       <button className="StartBtn" onClick={Loading}></button>
+    //       <button className="EndingCntBtn"></button>
+    //     </div>
+    //   </div>
+    // );
 
-    const Loading = () => {
-      setTimeout(()=>{
-          movePage('/question');
-      },2000);
+    const [isLoading, setIsLoading] = useState(false);
 
-      return <div className="loading"></div>
-    }
-  
+    const handleStartClick = () => {
+        setIsLoading(true);
+        setTimeout(() => {
+            movePage('/question');
+        }, 1000);
+    };
+
     return (
-      <div id="background">
-        <div className="btn">
-          <button className="StartBtn" onClick={Loading}></button>
-          <button className="EndingCntBtn"></button>
+        <div id="background">
+            <div className="btn">
+                <button className="StartBtn" onClick={handleStartClick}></button>
+                <button className="EndingCntBtn"></button>
+            </div>
+            {
+              isLoading && 
+              <div className="loading">
+                <img src="./images/김여주.gif"/>
+                <p className="loadingText">로딩중...</p>
+              </div>
+            }
         </div>
-      </div>
     );
   
 }
--- src/components/story1.js
+++ src/components/story1.js
@@ -4,14 +4,14 @@ import "./css/story1.css"
 export default function Story(){
     return(
         <div className="container">
-            <div className="kimyeoju">
-                <img src="./images/Kimyeoju_face1.png" className="kimyeojuImg"/>
-                <img src="./images/Kimyeoju_dialogueWindow1.png" className="dialogueWindow1"/>
+                {/* <img src="./images/Kimyeoju_face1.png" className="kimyeojuImg"/> */}
+                {/* <img src="./images/Kimyeoju_dialogueWindow1.png" className="dialogueWindow1"/> */}
+                <img src="./images/Baekleehyun_face1-1.png" className="BaekleehyunImg"/>
+                <img src="./images/Baekleehyun_dialogueWindow.png" className="dialogueWindow1"/>
                 <div className="nameAndDialogue">
                     <div className="name">김여주</div>
                     <div className="dialogue">방구를 뀐 적있나요?</div>
                 </div>
-            </div>
         </div>
     )
 }
