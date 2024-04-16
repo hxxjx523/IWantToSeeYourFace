8e178c14b93eead01bff3791e7bb4d1e66c9c3dc
hxxjx523 <hhj05230@gmail.com>
2024-04-16 09:43:34 +0900

    김여주 이름, 내용 추가

--- src/components/story1.js
+++ src/components/story1.js
@@ -7,8 +7,10 @@ export default function Story(){
             <div className="kimyeoju">
                 <img src="./images/Kimyeoju_face1.png" className="kimyeojuImg"/>
                 <img src="./images/Kimyeoju_dialogueWindow1.png" className="dialogueWindow1"/>
-                <div className="name">김여주</div>
-                <div className="dialogue">안녕하세요?</div>
+                <div className="nameAndDialogue">
+                    <div className="name">김여주</div>
+                    <div className="dialogue">방구를 뀐 적있나요?</div>
+                </div>
             </div>
         </div>
     )
