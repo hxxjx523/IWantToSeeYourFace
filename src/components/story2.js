18eb4dab7b8585c0906c5002e4ed9ee79074e134
hxxjx523 <hhj05230@gmail.com>
2024-04-17 14:03:47 +0900

    애니메이션 수정

--- src/components/story2.js
+++ src/components/story2.js
@@ -9,11 +9,8 @@ export default function Story2(){
         // 선택지 발생 이미지가 나온 후 3초 후에 이미지를 숨기고 버튼을 표시하기 위해 setTimeout 사용
         const timeout = setTimeout(() => {
             setShowImage(false);
-            // 선택지 발생 이미지가 완전히 사라진 후에 버튼을 표시하도록 setTimeout 사용
-            setTimeout(() => {
-                setShowButtons(true);
-            }, 1000); // 1초 뒤에 버튼을 표시
-        }, 3000); // 3초 뒤에 선택지 발생 이미지를 숨김
+            setShowButtons(true);
+        }, 2000);
 
         // 컴포넌트가 언마운트될 때 timeout을 클리어하여 메모리 누수를 방지
         return () => clearTimeout(timeout);
@@ -26,18 +23,20 @@ export default function Story2(){
     return (
         <div className="container2">
             {/* 실루엣 이미지 */}
-            <img src="./images/Baekleehyun_silhouette.png" className={`silhouette ${showImage ? "show" : ""}`} alt="실루엣" />
+            <img src="./images/Baekleehyun_silhouette.png" className="silhouette" alt="실루엣" />
 
-            {/* 선택지 발생 이미지 */}
-            {showImage && <img src="./images/선택지발생.png" className={`ss ${showImage ? "show" : ""}`} alt="선택지 발생" />}
+            <div className="selectImgDiv">
+                {/* 선택지 발생 이미지 */}
+                {showImage && <img src="./images/선택지발생.png" className={`selectImg ${showImage ? "show" : ""}`} alt="선택지 발생" />}  
+            </div>
 
             {/* 버튼 표시 여부에 따라 조건부 렌더링 */}
             {showButtons && (
                 <>
-                    <button className="btn" onClick={handleButtonClick}>
+                    <button className="selectButton" onClick={handleButtonClick}>
                         <img src="./images/StartBtn.png" alt="시작 버튼" />
                     </button>
-                    <button className="btn" onClick={handleButtonClick}>
+                    <button className="selectButton" onClick={handleButtonClick}>
                         <img src="./images/StartBtn.png" alt="시작 버튼" />
                     </button>
                 </>
