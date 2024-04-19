cc1526e34bbd6e54edc01dbe4f35171aa2af1923
yoonseo232 <ys.bang232@gmail.com>
2024-04-19 09:25:00 +0900

    이미지 변경 및 추가, 경로 변경

--- src/components/PhoneHome.js
+++ src/components/PhoneHome.js
@@ -6,10 +6,10 @@ export default function PhoneHome(){
         <div className="background">
 
         <div className="app">
-            <a href="/PhoneNumber"><button><img src="./images/phone_address.png" alt="" width="70px"/></button></a>
-            <a href=""><button><img src="./images/phone_call.png" alt="" width="70px"/></button></a>
-            <a href="message.html"><button><img src="./images/phone_message.png" alt="" width="70px"/></button></a>
-            <a href="gallery.html"><button><img src="./images/phone_gallery.png" alt="" width="70px"/></button></a>
+            <a href="/PhoneNumber"><button><img src="./images/phone_addressApp.png" alt="" width="70px"/></button></a>
+            <a href=""><button><img src="./images/phone_callApp.png" alt="" width="70px"/></button></a>
+            <a href="message.html"><button><img src="./images/phone_messageApp.png" alt="" width="70px"/></button></a>
+            <a href="gallery.html"><button><img src="./images/phone_galleryApp.png" alt="" width="70px"/></button></a>
         </div>
         </div>
     );
