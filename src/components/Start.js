ce2d9cac893834f807bbac720fe44b3c2c951f39
yoonseo232 <ys.bang232@gmail.com>
2024-05-20 08:33:44 +0900

    start 페이지 음악 넣기

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,16 +1,33 @@
-import { useNavigate } from "react-router-dom";
+import { useState, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import "./css/all.css";
 import styles from "./css/start.module.css";
 
 export default function Start() {
     const movePage = useNavigate();
+    const [audioPlayed, setAudioPlayed] = useState(false);
+    const audioRef = useRef(null);
+
+    const handleMusicClick = () => {
+        if (!audioPlayed) {
+            const audio = new Audio('./music/title_music.mp3'); 
+            audio.loop = true; 
+            audio.play(); 
+            audioRef.current = audio; 
+            setAudioPlayed(true); 
+        }
+    };
 
     const handleStartClick = () => {
+        audioRef.current.pause();
+        audioRef.current.currentTime = 0; 
+        setAudioPlayed(false); 
         movePage('/question');
-    };
+    }
 
     return (
         <div id={styles.background}>
+                <button className={styles.MusicBtn} onClick={handleMusicClick}><img src="./images/music-note-beamed.svg" alt="" width="30px"/></button>
             <div className={styles.btn}>
                 <button className={styles.StartBtn} onClick={handleStartClick}></button>
                 <button className={styles.EndingCntBtn}></button>
