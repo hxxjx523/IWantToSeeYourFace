c5840b0394b986f39f9b15818603e9db517f9685
yoonseo232 <ys.bang232@gmail.com>
2024-06-08 15:21:40 +0900

    핸드폰 전송 테스트 성공

--- src/components/Dialogue.js
+++ src/components/Dialogue.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import styles from "./css/Dialogue.module.css";
-
+import 
 function Dialogue({ name, text, img, window, background, isExiting }){
+    async function sendSign(){
+        try {
+            const response = await fetch('/send', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ message: 'sign' })
+            });
+
+            if (response.ok) {
+                console.log('전송선공');
+            } else {
+                console.error('전송실패');
+            }
+        } catch (error) {
+            console.error('에러 -> ', error);
+        }
+    }
+
+    if(text == "(핸드폰을 보자)"){
+        sendSign();
+    }
+
     return (
         <div className={`${styles.container1} ${isExiting ? styles.fadeOut : ''}`}>
             <img src={background} className={styles.background}/>
