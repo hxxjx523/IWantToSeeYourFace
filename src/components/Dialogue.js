fb9bb76e06f3d49f5fd42184df3539e5bc0344ce
heeju <hhj05230@gmail.com>
2024-06-15 20:26:40 +0900

    대사 컴포넌트 수정

--- src/components/Dialogue.js
+++ src/components/Dialogue.js
@@ -87,7 +87,7 @@ function Dialogue({ routeData, chapter, select1, select2, end, goodEnd, silhouet
     const advanceDialogue = useCallback(() => {
         setCurrentIndex(prevIndex => {
             if (showContainer2) {
-                return prevIndex; // showContainer2가 true인 경우 인덱스 증가하지 않음
+                return prevIndex;
             }
 
             const newIndex = Math.min(prevIndex + 1, routeData.length - 1);
@@ -113,22 +113,16 @@ function Dialogue({ routeData, chapter, select1, select2, end, goodEnd, silhouet
             // if (!confirmation && currentDialogue.text === "어??? 민들레?!!") return;
 
             if (key === 'Enter' || key === ' ' || key === 'ArrowRight') {
-                if (!isTyping) {
-                    advanceDialogue();
-                } else {
-                    setIsTyping(false); // 현재 타이핑 중이면 전체 텍스트를 바로 표시
-                }
+                advanceDialogue();
             } else if (key === 'ArrowLeft') {
                 retreatDialogue();
             }
         };
-
         document.addEventListener('keydown', handleKeyPress);
-
         return () => {
             document.removeEventListener('keydown', handleKeyPress);
         };
-    }, [advanceDialogue, retreatDialogue, confirmation, isTyping]);
+    }, [advanceDialogue, retreatDialogue, confirmation]);
 
     const getNextDialogue = () => {
         const nextDialogue = routeData[currentIndex];
@@ -151,32 +145,7 @@ function Dialogue({ routeData, chapter, select1, select2, end, goodEnd, silhouet
         }
     };
 
-    const typeText = useCallback((text) => {
-        setIsTyping(true);
-        let index = 0;
-        const interval = setInterval(() => {
-            if (!isTyping) {
-                clearInterval(interval);
-                setDisplayText(text);
-                return;
-            }
-            if (index < text.length) {
-                setDisplayText(prev => prev + text.charAt(index));
-                index++;
-            } else {
-                clearInterval(interval);
-                setIsTyping(false);
-            }
-        }, 50); // 글자가 출력되는 속도 조정
-    }, [isTyping]);
-
-    useEffect(() => {
-        const { text } = getNextDialogue();
-        setDisplayText(''); // 이전 텍스트 초기화
-        typeText(text);
-    }, [currentIndex, typeText]);
-
-    const { name, img, window, background } = getNextDialogue();
+    const { name, text, img, window, background } = getNextDialogue();
 
 
     //json의 text가 {픽셀이 지나가는 가는 효과}이면 픽셀 컴포넌트를 보여줌
@@ -235,11 +204,11 @@ function Dialogue({ routeData, chapter, select1, select2, end, goodEnd, silhouet
         }
     };
 
-    useEffect(() => {
-        if (text === "(핸드폰을 보자)") {
-            // sendSign();
-        }
-    }, [text]);
+    // useEffect(() => {
+    //     if (text === "(핸드폰을 보자)") {
+    //         // sendSign();
+    //     }
+    // }, [text]);
 
     //홈화면으로
     const goStart = () => {
@@ -257,7 +226,7 @@ function Dialogue({ routeData, chapter, select1, select2, end, goodEnd, silhouet
                         <img src={window} className={styles.dialogueWindow1}/>
                         <div className={styles.nameAndDialogue}>
                             <div className={styles.name}>{name}</div>
-                            <div className={styles.dialogue}>{displayText}</div>
+                            <div className={styles.dialogue}>{text}</div>
                             <button className={styles.next}></button>
                         </div>
                     </div>
