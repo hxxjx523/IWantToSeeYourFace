5d1c8abd7012e08411cdc8192aa9400e28631e60
heeju <hhj05230@gmail.com>
2024-06-16 22:16:02 +0900

    수정

--- src/components/BaekLeeHyun/BaekLeeHyunRoute1.js
+++ src/components/BaekLeeHyun/BaekLeeHyunRoute1.js
@@ -1,11 +1,32 @@
 import "../css/all.css";
 import baekLeeHyunRoute from '../json/baekLeeHyunRoute1.json'; // JSON 파일 경로에 맞게 수정
 import Dialogue from "../Dialogue";
+import { useEffect, useRef } from "react";
 
-function BaekLeeHyunRoute1(){
-    return (
-        <Dialogue routeData={baekLeeHyunRoute} chapter={"chapter2"}/>
-    );
+function BaekLeeHyunRoute1() {
+
+    const audioRef = useRef()
+
+    useEffect(() => {
+        if (audioRef.current) {
+        const playAudio = async () => {
+            try {
+            await audioRef.current.play();
+            } catch (error) {
+            console.error('자동 재생 실패:', error);
+            }
+        };
+        playAudio();
+        }
+    }, []);
+
+  return (
+    <>
+    
+      <audio ref={audioRef} src="./music/story_music.mp3" />
+      <Dialogue routeData={baekLeeHyunRoute} chapter={"chapter2"} />
+    </>
+  );
 }
 
 export default BaekLeeHyunRoute1;
--- src/components/DoYoon/BadCook.js
+++ src/components/DoYoon/BadCook.js
@@ -1,5 +1,5 @@
 // import "../css/all.css";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import End from '../json/BadCook.json'
 import EndingDialogue from '../EndingDialogue'
@@ -10,6 +10,7 @@ import Ending from '../Ending'
 function BadCook(){
 
     const [showEnding, setShowEnding] = useState(false);
+    const audioRef = useRef()
 
     const handleDialogueEnd = () => {
         setShowEnding(true);
@@ -19,8 +20,23 @@ function BadCook(){
     const endTitle = "END(5/9) - BAD COOK"
     const endContent = "이런.. 주방을 망치고 말았다"
 
+    useEffect(() => {
+        if (audioRef.current) {
+        const playAudio = async () => {
+            try {
+            await audioRef.current.play();
+            } catch (error) {
+            console.error('자동 재생 실패:', error);
+            }
+        };
+        playAudio();
+        }
+    }, []);
+
+
     return (
         <div>
+            <audio ref={audioRef} src="./music/END-BadCook_music.mp3" />
             {!showEnding ? (
                 <EndingDialogue routeData={End} onDialogueEnd={handleDialogueEnd} />
             ) : (
